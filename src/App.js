0963b580fcefa652814e82d1f7220d117e364f1b
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-16 21:52:43 -0700

    render login component on App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './Login.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HEADER - Scoop
+      </header>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
